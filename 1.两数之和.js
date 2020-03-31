54895f983f28a0d599d84440f608c0a8adf43b7d
sunkun <sk@360shop.cn>
2020-03-31 10:32:56 +0800

    feat: 改进两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -8,14 +8,22 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    let arr = []
-    nums.forEach((e, i) => {
-      if(arr.length>1)return false
-      for (let j = i+1; j < nums.length; j++) {
-        if (e + nums[j] === target) arr = [i, j]
-      }
-    })
-    return arr
+// var twoSum = function(nums, target) {
+//     let arr = []
+//     nums.forEach((e, i) => {
+//       if(arr.length>1)return false
+//       for (let j = i+1; j < nums.length; j++) {
+//         if (e + nums[j] === target) arr = [i, j]
+//       }
+//     })
+//     return arr
+// };
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const y = target - nums[i];
+    let index = nums.indexOf(y)
+    if (index !== i && ~index) {
+      return [i, index]
+    }
+  }
 };
-
