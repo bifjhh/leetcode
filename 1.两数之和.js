cedb1648e19c4c77069c398e7e087b5a6314b6bb
sunkun <sun_kun@venusgroup.com.cn>
2020-11-23 15:17:17 +0800

    refactor: 复习 两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -1,29 +1,31 @@
-/*
- * @lc app=leetcode.cn id=1 lang=javascript
- *
- * [1] 两数之和
- */
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
-// var twoSum = function(nums, target) {
-//     let arr = []
-//     nums.forEach((e, i) => {
-//       if(arr.length>1)return false
-//       for (let j = i+1; j < nums.length; j++) {
-//         if (e + nums[j] === target) arr = [i, j]
-//       }
-//     })
-//     return arr
-// };
-var twoSum = function (nums, target) {
+/*
+// 利用js  特性 arr.indexOf 查找, 有点取巧
+var twoSum = function(nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    const y = target - nums[i];
-    let index = nums.indexOf(y)
-    if (index !== i && ~index) {
+    const index = nums.indexOf(target - nums[i])
+    if(i!== index && ~index ) {
       return [i, index]
     }
+  }
+};
+*/
+// 通过map  一个 哈希表的意思, 获取两个索引
+
+var twoSum = function(nums, target) {
+  const map = {}
+  for (let i = 0; i < nums.length; i++) {
+    const v = nums[i]
+    // 得到  target - v 的差值
+    const num = target - v
+    // 如果这个差值在 map 中 已存在, 则return [map的索引, 当前索引]
+    if(map[num] !== undefined) {
+      return [map[num], i]
+    }
+    map[v] = i
   }
 };
