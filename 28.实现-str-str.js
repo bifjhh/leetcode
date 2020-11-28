a4a2de957295213bb8809aafb3fd477ab7807b36
bifjhh <929160004@qq.com>
2020-11-28 16:07:09 +0800

    refactor: 实现 strStr(): 直接indexOf了

--- 28.实现-str-str.js
+++ 28.实现-str-str.js
@@ -11,17 +11,6 @@
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  // needle 为空 则返回0
-  if (!needle) return 0
-  // 如果haystack为空, 或者 needle 的长度 超过haystack, 则返回 -1. 因为肯定包含不了吖
-  if (!haystack || needle.length > haystack.length) return -1
-  // 判断, haystack 是否包含 needle 的内容
-  if (haystack.includes(needle)) {
-    // 截取并返回开始位置
-    return haystack.indexOf(needle)
-  } else {
-    // 否则返回 -1
-    return -1
-  }
+  return haystack.indexOf(needle)
 };
 // @lc code=end
