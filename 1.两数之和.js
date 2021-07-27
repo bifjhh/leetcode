42d141d9d056dae604362f3d081344d7991c1e2f
bifjhh <929160004@qq.com>
2021-07-27 17:37:05 +0800

    review: 两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -1,8 +1,15 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
  */
+
+// @lc code=start
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
 /*
 // 利用js  特性 arr.indexOf 查找, 有点取巧
 var twoSum = function(nums, target) {
@@ -16,7 +23,7 @@ var twoSum = function(nums, target) {
 */
 // 通过map  一个 哈希表的意思, 获取两个索引
 
-var twoSum = function(nums, target) {
+/* var twoSum = function(nums, target) {
   const map = {}
   for (let i = 0; i < nums.length; i++) {
     const v = nums[i]
@@ -28,4 +35,21 @@ var twoSum = function(nums, target) {
     }
     map[v] = i
   }
+}; */
+
+// 2021年07月27日17:20:16
+var twoSum = function(nums, target) {
+  const map = {[nums[0]]: 0};
+  for (let i = 1; i < nums.length; i++) {
+    const n = nums[i];
+    const v = map[target-n];
+    if (v !== undefined) return [v, i];
+    map[n] = i;
+  }
 };
+// const nums = [2, 7, 11, 15];
+// const target = 9;
+// console.log(twoSum(nums, target));
+// @lc code=end
+// @lc code=end
+
