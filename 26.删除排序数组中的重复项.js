1ea75908820bd9afaafc77a5a7d7d9e1e527250d
bifjhh <929160004@qq.com>
2021-08-19 11:47:56 +0800

    review: 复习

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -20,16 +20,17 @@
 var removeDuplicates = function (nums) {
   if (nums.length == 0) return 0;
   // 慢指针 r
-  let r = 0
+  let r = 0, i = 1;
   //  循环, i 为快指针
-  for (let i = 0; i < nums.length; i++) {
+  while (i < nums.length) {
     // 如果 慢指针 r 索引 的数值 != 快指针 i 的数据, 则表示这个数据还没有出现过, 
-    if (nums[i] !== nums[r]) {
+    if (nums[r] !== nums[i]) {
       // 慢指针 +1
       r++
       // 将慢指针 r 索引的数据 修改为快指针的数据
       nums[r] = nums[i]
     }
+    i++;
     // 如果快指针的数据  nums[i] = nums[j] 则表示已经有了, 则不操作
     // 主要是给定的参数是已经排序过的数组, 所以开头第一个被慢指针记录过了, 相同的值也都是在第一个的后面
   }
