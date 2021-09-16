98fee1331ad5a6c92f16327e61d0d3ca48a73acc
bifjhh <929160004@qq.com>
2021-09-16 17:11:26 +0800

    HOT100:翻转二叉树

---
+++ Top100/226.翻转二叉树.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=javascript
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function (root) {
+  if (!root) return root;
+  let l = root.left,
+    r = root.right;
+  root.left = r;
+  root.right = l;
+  invertTree(l)
+  invertTree(r)
+  return root;
+};
+// @lc code=end
