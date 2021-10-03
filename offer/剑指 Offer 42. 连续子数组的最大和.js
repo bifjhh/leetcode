109200b00b8677e5473be1bb21ae9bd16c4cda8f
bifjhh <929160004@qq.com>
2021-10-03 09:59:16 +0800

    offer2: 连续子数组的最大和优化

--- offer/剑指 Offer 42. 连续子数组的最大和.js
+++ offer/剑指 Offer 42. 连续子数组的最大和.js
@@ -3,12 +3,16 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
+  if (nums.length == 1) return nums[0]
+  // 初始化 max为数组的第一个, pre 0
   let max = nums[0],
     pre = 0;
-  nums.forEach(e => {
-    pre = Math.max(e, pre + e)
-    max = Math.max(pre, max)
-  });
+  for (let i = 0; i < nums.length; i++) {
+    // pre = pre + e || e, 取最大值
+    pre = Math.max(pre + nums[i], nums[i])
+    // max 也一样, 如果pre相加, 比max 大,则保存, 否则不变
+    max = Math.max(max, pre)
+  }
   return max;
 };
 
