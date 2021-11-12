5ce2045c3d84d5873c823eb2119162601e99de6b
bifjhh <929160004@qq.com>
2021-11-12 18:18:35 +0800

    refactor: 三数之和

--- 15. 三数之和.js
+++ 15. 三数之和.js
@@ -1,34 +1,48 @@
 /**
  * @param {number[]} nums
+ * @param {number} target
  * @return {number[][]}
  */
-var threeSum = function (nums) {
-  let res = [],
-    len = nums.length - 1;
-  // 排序, 更容易计算
-  nums = nums.sort((a, b) => a - b);
-  for (let k = 0; k < len - 1; k++) {
-    // 当前值 大于0, 则后面所有的值, 都大于0, 因为是排序过了 
-    if (nums[k] > 0) break;
-    // 如果值相同, 则跳过
-    if (k && nums[k] == nums[k - 1]) continue;
-    // 双指针, l = k的下一个索引, r 每次从末端开始
-    let l = k + 1,
-      r = len;
+var threeSum = function (nums, target = 0) {
+  let res = [], len = nums.length - 1, l, r, sum;
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < len - 1; i++) {
+    if (nums[i] > target) return res;
+    if (i && nums[i] == nums[i - 1]) continue;
+    l = i + 1;
+    r = len;
     while (l < r) {
-      let sum = nums[k] + nums[l] + nums[r];
-      if (sum == 0) {
-        // =0的情况下 push结果
-        res.push([nums[k], nums[l], nums[r]])
-        // 跳过重复的值
+      sum = nums[i] + nums[l] + nums[r];
+      if (sum === target) {
+        res.push([nums[i], nums[l], nums[r]])
         while (l < r && nums[l] == nums[++l]);
         while (l < r && nums[r] == nums[--r]);
-      } else if (sum < 0) { // sum 小了, 则l 指针++到下一个不重复的值
+      } else if (sum < target) {
         while (l < r && nums[l] == nums[++l]);
-      } else { // sum 大了, 则r 指针--到下一个不重复的值
+      } else {
         while (l < r && nums[r] == nums[--r]);
       }
     }
   }
   return res;
 };
+
+
+var nums = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(nums));
+/* 
+给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+ 
+
+示例 1：
+
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+示例 2：
+
+输入：nums = []
+输出：[]
+*/
